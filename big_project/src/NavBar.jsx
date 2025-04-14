412077948a8f3b6258d54fcbe1dd4f8300d8eb7d
Kaszás Szófia <szofia.kaszas@gmail.com>
2025-04-14 23:16:54 +0200

    changed colors a bit and put a search bar on navbar (+ some link but just for looks)

--- big_project/src/NavBar.jsx
+++ big_project/src/NavBar.jsx
@@ -1,18 +1,30 @@
-export default function NavBar(){
+export default function NavBar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="index.html">Home</a>
-                        </li>
-                    </ul>
-                </div>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <span class="navbar-brand">Video lejatszo</span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">idk</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">idk</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">idk</a>
+                    </li>
+                </ul>
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                    <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+                </form>
             </div>
         </nav>
     )

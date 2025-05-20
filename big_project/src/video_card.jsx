f5ed5d780bdc86edf5b2596f0f7692634cedc620
Kaszás Szófia <szofia.kaszas@gmail.com>
2025-05-20 19:24:18 +0200

    Made video card clickable and look better

--- big_project/src/video_card.jsx
+++ big_project/src/video_card.jsx
@@ -1,10 +1,14 @@
+import "./videoCard.css";
+
 export default function Video_card(props){
     return (
-        <div className="card" style={{ width: "18rem" }}>
+        <a className="basicLink" href={props.link} target="_blank">
+        <div className="card">
             <img src={props.video_cover_img} className="card-img-top" alt={props.video_cover_alt}></img>
             <div className="card-body">
                 <p className="card-title">{props.video_title}</p>
             </div>
         </div>
+        </a>    
     )
 }

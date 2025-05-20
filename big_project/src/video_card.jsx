a42c2d2e0219acc41affa460872d66624f01fc10
Kaszás Szófia <szofia.kaszas@gmail.com>
2025-05-20 20:31:07 +0200

    fixed look issues that .map caused

--- big_project/src/video_card.jsx
+++ big_project/src/video_card.jsx
@@ -3,7 +3,7 @@ import "./videoCard.css";
 export default function Video_card(props){
     return (
         <a className="basicLink" href={props.link} target="_blank">
-        <div className="card">
+        <div className="card video-card">
             <img src={props.video_cover_img} orientation="top" className="card-img-top" alt={props.video_cover_alt}></img>
             <div className="card-body">
                 <p className="card-title">{props.video_title}</p>

3f70509a695fd6f4485f06af80865703ea8f7ca9
Kaszás Szófia <szofia.kaszas@gmail.com>
2025-05-20 20:44:23 +0200

    #

--- big_project/src/App.jsx
+++ big_project/src/App.jsx
@@ -1,18 +1,22 @@
+import { useState } from 'react'
+
 import './index.css'
 import NavBar from './NavBar.jsx'
 import Video_card from './video_card.jsx'
-import IFrame from './iFrame.jsx'
 import videoData from './videos.json'
 
 function App() {
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
   return (
     <>
       <NavBar />
       <div className='container-fluid'>
         <div className='row'>
           {videoData.map((video, index) => (
-            <div key={index} className="col-md-3">
+            <div key={index} className="col-md-3 mb-4">
               <Video_card
+                onClick={() => setSelectedIndex(index)}
                 link={video.link}
                 video_cover_img={video.thumbnail}
                 video_cover_alt={video.alt}

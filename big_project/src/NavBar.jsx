0db4cb1e65168ff78eb2c38c87139923e12feefc
Kaszás Szófia <szofia.kaszas@gmail.com>
2025-05-20 19:27:23 +0200

    adjusted things so the code could be cleaner

--- big_project/src/NavBar.jsx
+++ big_project/src/NavBar.jsx
@@ -1,3 +1,5 @@
+import "./navBar.css";
+
 export default function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
--- big_project/src/main.jsx
+++ big_project/src/main.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './navbar.css'
 import NavBar from './NavBar.jsx'
 import Video_card from './video_card.jsx'
+import IFrame from './iFrame.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NavBar/>
     <div className='container-fluid'>
       <div className='row'>
+        <Video_card video_cover_img = "#" video_cover_alt = "fasza video" video_title = "title"/>
+        <Video_card video_cover_img = "#" video_cover_alt = "fasza video" video_title = "title"/>
         <Video_card video_cover_img = "#" video_cover_alt = "fasza video" video_title = "title"/>
         <Video_card video_cover_img = "#" video_cover_alt = "fasza video" video_title = "title"/>
         <Video_card video_cover_img = "#" video_cover_alt = "fasza video" video_title = "title"/>

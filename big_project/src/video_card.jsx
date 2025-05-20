39a2e8ed517665e3225bd1b545eeeb45cdbc3d9c
Kaszás Szófia <szofia.kaszas@gmail.com>
2025-05-20 20:06:58 +0200

    adjusted the width and height of the cards so the thumbnails could fit

--- big_project/src/video_card.jsx
+++ big_project/src/video_card.jsx
@@ -4,7 +4,7 @@ export default function Video_card(props){
     return (
         <a className="basicLink" href={props.link} target="_blank">
         <div className="card">
-            <img src={props.video_cover_img} className="card-img-top" alt={props.video_cover_alt}></img>
+            <img src={props.video_cover_img} orientation="top" className="card-img-top" alt={props.video_cover_alt}></img>
             <div className="card-body">
                 <p className="card-title">{props.video_title}</p>
             </div>

04f99000bcf90f6c541d1b9bd76538159a8e87ba
Kaszás Szófia <szofia.kaszas@gmail.com>
2025-05-21 10:07:07 +0200

    fixed problems with useState and loaded in the iFrame api used it to load a video

--- big_project/src/App.jsx
+++ big_project/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useEffect, useRef } from 'react';
 
 import './index.css'
 import NavBar from './NavBar.jsx'
@@ -7,28 +8,85 @@ import videoData from './videos.json'
 
 function App() {
   const [selectedIndex, setSelectedIndex] = useState(null);
+  console.log(selectedIndex);
 
   return (
     <>
       <NavBar />
       <div className='container-fluid'>
         <div className='row'>
-          {videoData.map((video, index) => (
-            <div key={index} className="col-md-3 mb-4">
-              <Video_card
-                onClick={() => setSelectedIndex(index)}
-                link={video.link}
-                video_cover_img={video.thumbnail}
-                video_cover_alt={video.alt}
-                video_title={video.title}
-              />
-            </div>
-          ))}
-
+          <NewIndex selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
         </div>
       </div>
     </>
   )
+}
+
+function NewIndex({ selectedIndex, setSelectedIndex }) {
+  if (selectedIndex !== null) {
+    const selectedVideo = videoData[selectedIndex];
+    return (
+      <>
+        <div id="player"></div>
+        <YouTubePlayer videoId={selectedVideo.link} />
+      </>
+    )
+  }
+  else {
+    return (
+      <>
+        {videoData.map((video, index) => (
+          <div key={index} className="col-md-3 mb-4">
+            <Video_card
+              onClick={() => setSelectedIndex(index)}
+              video_cover_img={video.thumbnail}
+              video_cover_alt={video.alt}
+              video_title={video.title}
+            />
+          </div>
+        ))}
+      </>
+    )
+  }
+}
+
+function YouTubePlayer({ videoId }) {
+  const playerRef = useRef(null);
+
+  useEffect(() => {
+    if (!window.YT) {
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      const firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      window.onYouTubeIframeAPIReady = loadPlayer;
+    } else {
+      loadPlayer();
+    }
+
+    function loadPlayer() {
+      new window.YT.Player(playerRef.current, {
+        height: '730',
+        width: '980',
+        videoId: videoId,
+        playerVars: { playsinline: 1 },
+        events: {
+          onReady: (event) => event.target.playVideo(),
+        }
+      });
+    }
+
+    function onPlayerStateChange(event) {
+      if (event.data === window.YT.PlayerState.PLAYING) {
+        setTimeout(() => {
+          event.target.stopVideo();
+        }, 6000);
+      }
+    }
+  }, [videoId]);
+
+  return <div id="player" ref={playerRef}></div>;
 }
 
 export default App
--- big_project/src/video_card.jsx
+++ big_project/src/video_card.jsx
@@ -2,7 +2,7 @@ import "./videoCard.css";
 
 export default function Video_card(props){
     return (
-        <a className="basicLink" href={props.link} target="_blank">
+        <a className="basicLink" target="_blank" onClick={props.onClick}>
         <div className="card video-card">
             <img src={props.video_cover_img} orientation="top" className="card-img-top" alt={props.video_cover_alt}></img>
             <div className="card-body">

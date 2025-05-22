d1c4cdc40928a7d37368517dd96ec58fe07e7ce5
Rikimik06 <tothpatrikpeter@gmail.com>
2025-05-22 18:03:28 +0200

    styled the navbar, renamed the nav links

--- big_project/src/NavBar.jsx
+++ big_project/src/NavBar.jsx
@@ -1,33 +1,49 @@
 import "./navBar.css";
 
 export default function NavBar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light">
-            <span className="navbar-brand">Video lejatszo</span>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm px-4 py-3">
+      <a className="navbar-brand font-weight-bold" href="#">
+        🎬 Video Lejátszó
+      </a>
 
-            <div className="collapse navbar-collapse" id="navbarColor01">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <a className="nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">idk</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">idk</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">idk</a>
-                    </li>
-                </ul>
-                <form className="form-inline">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-                    <button className="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
-    )
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+                <a className="nav-link" href="index.html">Főoldal <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Kategóriák</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Újdonságok</a>
+            </li>
+        </ul>
+
+        <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Keresés..."
+            aria-label="Keresés"
+          />
+          <button className="btn btn-info my-2 my-sm-0" type="submit">
+            Keresés
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
 }
